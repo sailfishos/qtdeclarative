57a825c55875af675841e4978eb70f438c2dc81c
Carsten Munk <carsten.munk@jollamobile.com>
2013-07-15 12:11:37 +0000

    Squashed 'qtdeclarative/' changes from e4fc75e..811337a
    811337a FileDialog.folder property should also be a QUrl, for consistency
    87a4a3d QtQml: document QQmlExtensionPlugin::baseUrl
    74438ef QtQuick.Dialogs: sync the docs with the implementation
    a8e904c Switch to non-streaming version of qWarning()
    a73aae2 Undo presumably accidental line deletion
    98a0fa0 photosurface: add C++ boilerplate launcher; open button
    4e2153a Declarative dialog improvements for the non-Window use case
    a4754a3 Update 5.1.0 changelog and what's new
    cf01eac QQmlPlatform: rename "mac" to "osx"
    55ba0b9 Add missing Q_REVISION
    24947fb QColorDialogHelper: Force QWindow before setting flags
    d6dc07f Doc: Changed the title to avoid name conflicts and linking errors
    9f661cf photosurface demo: behavior depends on initialization order
    4455db6 stabilize tst_touchmouse.
    e2da7e6 QFileDialogHelper: Force QWindow before setting flags.
    278139c move quick dialogs example into an artificial subdirectory again
    5ce41d1 Clean up qt quick dialogs example again
    f64704f prepare for qt_module_pris.prf changes
    5c7f570 Fix compiler warning about unused parameter
    b45528f Doc: Exclude Qt Quick Dialogs examples in qtquick.qdocconf file
    
    git-subtree-dir: qtdeclarative
    git-subtree-split: 811337a73c160447e5218ae55e93df99ac3d9edd

--- examples/quick/dialogs/dialogs.pro
+++ examples/quick/dialogs/dialogs.pro
@@ -1,4 +1,4 @@
 TEMPLATE = subdirs
 
 SUBDIRS = \
-    colorandfiledialogs
+    systemdialogs
--- examples/quick/dialogs/systemdialogs/main.cpp
+++ examples/quick/dialogs/systemdialogs/main.cpp
@@ -38,4 +38,4 @@
 **
 ****************************************************************************/
 #include "../../shared/shared.h"
-DECLARATIVE_EXAMPLE_MAIN(dialogs/dialogs)
+DECLARATIVE_EXAMPLE_MAIN(dialogs/systemdialogs)
