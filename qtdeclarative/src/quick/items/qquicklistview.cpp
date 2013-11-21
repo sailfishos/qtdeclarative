sailfishos/qtdeclarative

commit a9f924975fbd78d67c8edc185767c9f2f19d6c27

Change sections attached properties atomically.
section, previousSection and nextSection attached properties usually
change together and are are often used together in an expression. Having
them change one at a time results in multiple re-evaluations with
differing results. This can be easily avoided.

qtdeclarative/src/quick/items/qquicklistview.cpp
@@ -575,20 +575,22 @@ FxViewItem *QQuickListViewPrivate::newViewItem(int modelIndex, QQuickItem *item)
 
     // initialise attached properties
     if (sectionCriteria) {
-        QString propValue = model->stringValue(modelIndex, sectionCriteria->property());
-        listItem->attached->setSection(sectionCriteria->sectionString(propValue));
+        QString section = model->stringValue(modelIndex, sectionCriteria->property());
+        QString prevSection;
+        QString nextSection;
         if (modelIndex > 0) {
             if (FxViewItem *item = itemBefore(modelIndex))
-                listItem->attached->setPrevSection(item->attached->section());
+                prevSection = item->attached->section();
             else
-                listItem->attached->setPrevSection(sectionAt(modelIndex-1));
+                prevSection = sectionAt(modelIndex-1);
         }
-        if (modelIndex < model->count()-1) {
-            if (FxViewItem *item = visibleItem(modelIndex+1))
-                listItem->attached->setNextSection(static_cast<QQuickListViewAttached*>(item->attached)->section());
+        if (modelIndex < model->count()) {
+            if (FxViewItem *item = visibleItem(modelIndex))
+                nextSection = static_cast<QQuickListViewAttached*>(item->attached)->section();
             else
-                listItem->attached->setNextSection(sectionAt(modelIndex+1));
+                nextSection = sectionAt(modelIndex);
         }
+        listItem->attached->setSections(prevSection, section, nextSection);
     }
 
     return listItem;

qtdeclarative/src/quick/items/qquickitemview_p.h
@@ -324,6 +324,21 @@ public:
         }
     }
 
+    void setSections(const QString &prev, const QString &sect, const QString &next) {
+        bool prevChanged = prev != m_prevSection;
+        bool sectChanged = sect != m_section;
+        bool nextChanged = next != m_nextSection;
+        m_prevSection = prev;
+        m_section = sect;
+        m_nextSection = next;
+        if (prevChanged)
+            emit prevSectionChanged();
+        if (sectChanged)
+            emit sectionChanged();
+        if (nextChanged)
+            emit nextSectionChanged();
+    }
+
     void emitAdd() { emit add(); }
     void emitRemove() { emit remove(); }
 

commit 5fe3f431f2f86ef0315c1e1e4f1977859c52073a

[qtdeclarative] Fix FirstCharacter section criteria.
Regression introduced by a9f924975fbd78d67c8edc185767c9f2f19d6c27.

qtdeclarative/src/quick/items/qquicklistview.cpp
@@ -575,7 +575,8 @@ FxViewItem *QQuickListViewPrivate::newViewItem(int modelIndex, QQuickItem *item)
 
     // initialise attached properties
     if (sectionCriteria) {
-        QString section = model->stringValue(modelIndex, sectionCriteria->property());
+        QString propValue = model->stringValue(modelIndex, sectionCriteria->property());
+        QString section = sectionCriteria->sectionString(propValue);
         QString prevSection;
         QString nextSection;
         if (modelIndex > 0) {