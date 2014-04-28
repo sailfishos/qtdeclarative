6c27505fd4aaab1d563cdda825a69ff49a19dff0
Gunnar Sletta <gunnar.sletta@jollamobile.com>
2014-04-28 13:30:04 +0200

    Say hello to QQuickItem::grabToImage().
    [ChangeLog][QtQuick][Item] Added functions QQuickItem::grabToImage()
    and Item::grabToImage() to allow grabbing of items into
    system-memory images.
    
    Change-Id: I76cd73bb62f7440569c6fce63d63528559845721
    Reviewed-by: Michael Brasser <michael.brasser@live.com>

--- src/quick/items/qquickitemsmodule.cpp
+++ src/quick/items/qquickitemsmodule.cpp
@@ -191,6 +191,7 @@ static void qt_quickitems_defineModule(const char *uri, int major, int minor)
     qmlRegisterType<QQuickTextInput,2>(uri,2,2,"TextInput");
     qmlRegisterType<QQuickViewSection>(uri,major,minor,"ViewSection");
 
+    qmlRegisterType<QQuickItemGrabResult>();
     qmlRegisterType<QQuickItemLayer>();
     qmlRegisterType<QQuickAnchors>();
     qmlRegisterType<QQuickKeyEvent>();
